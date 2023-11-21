b602ab2ec747456e4410b3fab210baf1974fe7d3
kristiandavidd <kristiandavid644@gmail.com>
2023-11-21 13:09:09 +0700

    fix: linkcard

--- resources/js/Pages/MyFile.jsx
+++ resources/js/Pages/MyFile.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Head } from '@inertiajs/react'
 import Navbar from '@/Components/navbar'
+import LinkCard from '@/Components/LinkCard'
 
 export default function MyFile({ auth }) {
     return (
         <>
             <Head title='File Saya'></Head>
             <Navbar auth={auth} />
+            <div className='grid grid-flow-row grid-cols-1 gap-4 px-10 py-4 sm:grid-cols-2 lg:grid-cols-3'>
+                <LinkCard />
+                <LinkCard />
+                <LinkCard />
+                <LinkCard />
+            </div>
         </>
 
     )
--- resources/js/Pages/Sampah.jsx
+++ resources/js/Pages/Sampah.jsx
@@ -1,12 +1,18 @@
 import { Head } from "@inertiajs/react"
 import Navbar from "@/Components/navbar"
+import LinkCard from "@/Components/LinkCard"
 
 export default function Sampah({ auth }) {
     return (
         <>
             <Head title="Sampah"></Head>
             <Navbar auth={auth} />
-            <h1>AowkOAKOKWOKAOKWOPKOAWKOKWA</h1>
+            <div className='grid grid-flow-row grid-cols-1 gap-4 px-10 py-4 sm:grid-cols-2 lg:grid-cols-3'>
+                <LinkCard />
+                <LinkCard />
+                <LinkCard />
+                <LinkCard />
+            </div>
         </>
     )
 }
