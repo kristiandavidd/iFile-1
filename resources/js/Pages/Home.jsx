a427476341ce1c785474baf476dcbda6838e5957
kristiandavidd <kristiandavid644@gmail.com>
2023-11-21 13:16:20 +0700

    fix: home

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -11,7 +11,7 @@ export default function Home({ auth }) {
         <>
             <Head title="Home" />
             <Navbar auth={auth} />
-            <div className='grid grid-flow-row grid-cols-1 gap-4 px-10 py-4 sm:grid-cols-3'>
+            <div className='grid grid-flow-row grid-cols-1 gap-4 px-10 py-4 sm:grid-cols-2 lg:grid-cols-3'>
                 <LinkCard />
                 <LinkCard />
                 <LinkCard />
