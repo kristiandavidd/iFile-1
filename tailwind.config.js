2c2c227037e831a7106dd10d5fa9bba11dd38502
kristiandavidd <kristiandavid644@gmail.com>
2023-12-29 15:58:11 +0700

    fix color

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,8 +16,8 @@ export default {
             'i-yellow-500': '#FF9800',
             'i-orange-500': '#F44336',
             'i-pink-500': '#E91E63',
-            'i-pink-100': '#F8B9CF',
             'i-pink-300': '#F06896',
+            'i-pink-100': '#F8B9CF',
             'i-pink-50': '#FDE9EF',
             'white': '#ffffff',
         },
@@ -29,4 +29,10 @@ export default {
     },
 
     plugins: [forms],
+    safelist: [
+        {
+            pattern:
+                /(bg|text|border)-(transparent|current|white|i-yellow-500|i-orange-500|i-pink-500|i-pink-300|i-pink-100|i-pink-50)/,
+        },
+    ],
 };
